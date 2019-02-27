5529fd9799467ba8c319835c24835695a35e710f
Alius C <alius.cesnavicius@megodata.com>
2019-02-27 23:02:22 +0200

    DB updating to work with Heroku

--- app/config/database.php
+++ app/config/database.php
@@ -1,7 +1,15 @@
 <?php
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_DATABASE', 'kirpykla');
+
+//define('DB_HOST', 'localhost');
+//define('DB_USER', 'root');
+//define('DB_PASS', '');
+//define('DB_DATABASE', 'kirpykla');
+//define('DB_CHARSET', 'utf8_binnacry_ci');
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+define('DB_HOST', $url["host"]);
+define('DB_USER', $url["user"]);
+define('DB_PASS', $url["pass"]);
+define('DB_DATABASE', substr($url["path"], 1);
 define('DB_CHARSET', 'utf8_binnacry_ci');
